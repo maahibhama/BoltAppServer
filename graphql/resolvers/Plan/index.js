91f1d2dae42ce536c8f72cb6059833b147bd536d
Mukesh Soni <msony552@gmail.com>
2021-04-24 12:37:56 +0530

    Plan mutations

---
+++ graphql/resolvers/Plan/index.js
@@ -0,0 +1,50 @@
+import Plan from "../../../models/Plan";
+var mongodb = require("mongodb");
+export default {
+  Mutation: {
+    addPlan: (root, { name, description, price, rating, rating_comments }) => {
+      const newPlan = Plan({
+        name,
+        description,
+        price,
+        rating,
+        rating_comments,
+      });
+
+      return new Promise((resolve, reject) => {
+        Plan.findOne({ name }).exec((err, res) => {
+          if (err) {
+            reject(err);
+          } else if (res === null) {
+            newPlan.save((err, res) => {
+              err ? reject(err) : resolve(res);
+            });
+          } else {
+            reject("Something went wrong while adding Plan");
+          }
+        });
+      });
+    },
+    editPlan: (root, args) => {
+      return new Promise((resolve, reject) => {
+        const name = args.name;
+        delete args.name;
+
+        Plan.findOneAndUpdate(
+          { _name: name },
+          { $set: args },
+          { new: true }
+        ).exec((err, res) => {
+          err ? reject(err) : resolve(res);
+        });
+      });
+    },
+    deletePlan: (root, args) => {
+      return new Promise((resolve, reject) => {
+        Plan.findOneAndRemove(args).exec((err, res) => {
+          err ? reject(err) : resolve(res);
+        });
+      });
+    },
+  },
+};
